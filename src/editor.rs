58e3c0831001b34a26ec7b6431ba4bce5f4886ee
ArminGEtemad <a.ghetemad@gmail.com>
2025-10-06 21:02:34 +0200

    Added editing the document and Ctrl+Q to exit

--- src/editor.rs
+++ src/editor.rs
@@ -1,5 +1,5 @@
 use crate::{document::Document, screen::Screen};
-use crossterm::{cursor::Show, event::{self, Event, KeyCode, KeyEvent}};
+use crossterm::{cursor::Show, event::{self, Event, KeyCode, KeyEvent, KeyModifiers}};
 use std::io::{self};
 
 const HORIZONTAL_OFFSET: u16 = 6; // supporting under 10k lines for now
@@ -44,9 +44,11 @@ impl Editor {
 
     fn handle_input(&mut self) -> io::Result<bool> {
         if event::poll(std::time::Duration::from_millis(500))? {
-            if let Event::Key(KeyEvent { code, .. }) = event::read()? {
+            if let Event::Key(KeyEvent { code, modifiers, .. }) = event::read()? {
                 match code {
-                    KeyCode::Char('q') => return Ok(true), 
+                    KeyCode::Char('q') if modifiers.contains(KeyModifiers::CONTROL)=> {
+                        return Ok(true);
+                    } 
                     KeyCode::Up => {
                         if self.cursor_y > 0 {
                             self.cursor_y -= 1;
@@ -67,6 +69,41 @@ impl Editor {
                             if self.cursor_x < line.len() {
                                 self.cursor_x += 1;
                             }
+                        }
+                    }
+                    KeyCode::Char(c) => {
+                        if let Some(line) = self.doc.lines.get_mut(self.cursor_y) {
+                            if self.cursor_x < line.len() {
+                                line.insert(self.cursor_x, c);
+                                self.cursor_x += 1;
+                            }
+                        }
+                    }
+                    KeyCode::Backspace => {
+                        if self.cursor_x > 0 {
+                            if let Some(line) = self.doc.lines.get_mut(self.cursor_y) {
+                                if self.cursor_x < line.len() {
+                                    line.remove(self.cursor_x - 1);
+                                    self.cursor_x -= 1;
+                                }
+                            }
+                        } else if self.cursor_y > 0 {
+                            // merging lines
+                            let current = self.doc.lines.remove(self.cursor_y);
+                            self.cursor_y -= 1;
+
+                            if let Some(prev) = self.doc.lines.get_mut(self.cursor_y) {
+                                self.cursor_x = prev.len();
+                                prev.push_str(&current);
+                            }
+                        }
+                    }
+                    KeyCode::Enter => {
+                        if let Some(line) = self.doc.lines.get_mut(self.cursor_y) {
+                            let new_line = line.split_off(self.cursor_x);
+                            self.cursor_y += 1;
+                            self.cursor_x = 0;
+                            self.doc.lines.insert(self.cursor_y, new_line);
                         }
                     }
                     _ => {}
