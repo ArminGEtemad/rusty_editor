665c9e3ffdacef3936a52fcea1dfa6866f15ea55
ArminGEtemad <a.ghetemad@gmail.com>
2025-10-06 19:59:41 +0200

    Cleaning up the code

--- src/document.rs
+++ src/document.rs
@@ -1,33 +1,20 @@
 use std::fs;
-use std::io; // io needed for input, output errors
+use std::io;
 
-// a struct to hold the content
 pub struct Document {
-    pub lines: Vec<String>, // each line stored as a separate string
+    pub lines: Vec<String>,
 }
 
-// implementing Document method
 impl Document {
     pub fn open(filename: &str) -> Result<Self, io::Error> {
-                                // If everything goes well the result is Self
-                                // If it fails we get io::Error
-        let contents = fs::read_to_string(filename)?; // ? give the value if 
-                                                      // Result is Ok and stop 
-                                                      // right here if Err
+        let contents = fs::read_to_string(filename)?; 
         let lines = contents
-            .lines() // spliting the file into lines
-            .map(|line| line.to_string()) // converts each borrowed &str into owned String
-            .collect(); // collecting into a vector
+            .lines() 
+            .map(|line| line.to_string()) 
+            .collect();
 
         Ok(Self { lines })
     }
-    /*
-    pub fn display(&self) {
-        for (i, line) in self.lines.iter().enumerate() {
-            println!("{:>4} | {}", i + 1, line);
-        }
-    }
-     */
 
     pub fn lines(&self) -> &[String] {
         &self.lines
